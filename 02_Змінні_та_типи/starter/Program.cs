6125fcb05de55e2357e361a458def2bcb040e779
Maksym Vasylyshyn <maxim24152771@gmail.com>
2026-09-04 00:34:02 +0300

    02_Data-Types-and-Structures

---
+++ 02_Змінні_та_типи/starter/Program.cs
@@ -0,0 +1,31 @@
+// Урок 2. Starter: змінні та типи даних.
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// TODO 1: Створіть 4 змінні для свого «паспорту»:
+// - name (string)
+// - age (int)
+// - height (double)
+// - isFemale (bool)
+
+
+// TODO 2: Виведіть один рядок через інтерполяцію:
+// "Імʼя: <name>, Вік: <age>, Зріст: <height>, Стать (true=Ж): <isFemale>"
+
+
+// TODO 3: Створіть змінну counter = 0, виведіть її,
+// потім присвойте counter = 100, виведіть знову.
+
+
+// TODO 4: Прочитайте на сторінці exercises.md "Завдання 4 - Площа кімнати".
+// Створіть length, width (double), обчисліть area = length * width.
+// Виведіть результат.
+
+
+// TODO 5 (бонус): Спробуйте var x = 5; потім x = "текст";
+// Що скаже компілятор? Запишіть у коментарі.
+
+
+Console.WriteLine();
+Console.WriteLine("Натисніть будь-яку клавішу...");
+Console.ReadKey();
