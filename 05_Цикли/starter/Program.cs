4c0b78d9761779e2e3008d8ec5b009ea446b51f7
Maksym Vasylyshyn <maxim24152771@gmail.com>
2026-09-06 00:50:43 +0300

    05_Cycles

---
+++ 05_Цикли/starter/Program.cs
@@ -0,0 +1,23 @@
+// Урок 5. Starter: цикли.
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// TODO 1: Цикл for виведе числа 1..10 по одному на рядку.
+
+
+// TODO 2: Зворотний відлік 10..1, потім "Старт!"
+
+
+// TODO 3: Прочитайте N. Обчисліть 1+2+...+N через цикл.
+
+
+// TODO 4: Таблиця множення на 7 (від 7*1 до 7*10).
+
+
+// TODO 5 (бонус): Гра "Вгадай число" 1..100 (див. exercises.md).
+// Підказка: var rnd = new Random(); int secret = rnd.Next(1, 101);
+
+
+Console.WriteLine();
+Console.WriteLine("Натисніть будь-яку клавішу...");
+Console.ReadKey();
