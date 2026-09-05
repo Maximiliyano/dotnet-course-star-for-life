bd01d75f6299aeba234a9371a6853407fd93302d
Maksym Vasylyshyn <maxim24152771@gmail.com>
2026-09-06 00:48:50 +0300

    03_Operators-and-Inputs

---
+++ 03_Оператори_та_введення/starter/Program.cs
@@ -0,0 +1,24 @@
+// Урок 3. Starter: ввід, оператори, перетворення.
+
+using System.Globalization;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+// TODO 1: Запитайте у користувача імʼя і вік.
+// Виведіть: "Привіт, <імʼя>! Через рік вам буде <вік+1>."
+
+
+// TODO 2: Прочитайте 2 цілі числа. Виведіть суму, різницю, добуток, частку.
+
+
+// TODO 3: Запитайте вагу і зріст. Обчисліть ІМТ.
+// Виведіть, заокруглений до 1 знака після коми.
+
+
+// TODO 4 (бонус): Перевірте, чи число парне (n % 2 == 0).
+
+
+Console.WriteLine();
+Console.WriteLine("Натисніть будь-яку клавішу...");
+Console.ReadKey();
